2a585ae8bb5a405adf60dcc83e7345b0323d5414
Romain Gadani <romain.gadani@gmail.com>
2021-12-14 11:57:23 +0100

    init liste carte ok

--- code/main.c
+++ code/main.c
@@ -2,67 +2,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "struct.h"
 
-typedef struct Card sCard;
-struct Card {
-    int Number;         // 1 to 13
-    char Symbol;        // carreau or pic or trefle or coeur
-    int ReturnedCard;   // 0 or 1
-    int CardSlot;       // 0 or 1 or 2
-    int IdCard;         // 1 to 52
-    sCard* RightChild;  // FALSE or &AdresseMemoire
-    sCard* LeftChild;   // FALSE or &AdresseMemoire
-    sCard* RightParent; // FALSE or &AdresseMemoire
-    sCard* LeftParent;  // FALSE or &AdresseMemoire
-    sCard* Next;        // FALSE or &AdresseMemoire
-    sCard* Previous;    // FALSE or &AdresseMemoire
-};
-
-
-struct TreeCards {
-    struct sCard* IdCard;
-    sCard* FirstRightImaginary;   // &AdresseMemoire
-    sCard* FirstLeftImaginary;    // &AdresseMemoire
-};
-typedef struct TreeCards sTreeCards;
-
-struct ListCards {
-    sCard* First;       // &AdresseMemoire
-    sCard* Last;        // &AdresseMemoire
-    int NumberElements; // 1 to (max) 52
-};
-typedef struct ListCards  sListCards;
-
-
-void InitialisationCards(sCard *tab) {
-    char symboltab[4] = { 'c', 'p', 't', 'h' };
-
-    int i = 1;
-    int x = 0;
-
-    while (i <= 13) {
-        int j = 0;
-        while (j <= 3) {
-            tab[x].Symbol = symboltab[j];
-            tab[x].Number = i;
-            tab[x].IdCard = x;
-            tab[x].ReturnedCard = 0;
-            tab[x].CardSlot = 0;
-            tab[x].RightChild = NULL;
-            tab[x].LeftChild = NULL;
-            tab[x].RightParent = NULL;
-            tab[x].LeftParent = NULL;
-            tab[x].Next = NULL;
-            tab[x].Previous = NULL;
-            printf("%d, %c\n", tab[x].Number, tab[x].Symbol);
-            j += 1;
-            x += 1;
-        }
-        i += 1;    
-    }
-}
 
 int main(void) {
     sCard tab[52];
     InitialisationCards(&tab);
 }
+
