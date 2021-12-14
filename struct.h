7dddc17863e277a4976301de20da200ef0c80064
Romain Gadani <romain.gadani@gmail.com>
2021-12-14 14:19:43 +0100

    mise a jour

--- struct.h
+++ struct.h
@@ -1,29 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Card sCard;
 struct Card {
     int Number;         // 1 to 13
-    char* Symbol;       // carreau or pic or trefle or coeur
+    char Symbol;        // carreau or pic or trefle or coeur
     int ReturnedCard;   // 0 or 1
     int CardSlot;       // 0 or 1 or 2
     int IdCard;         // 1 to 52
-    char RightChild;    // FALSE or &AdresseMemoire
-    char LeftChild;     // FALSE or &AdresseMemoire
-    char RightParent;   // FALSE or &AdresseMemoire
-    char LeftParent;    // FALSE or &AdresseMemoire
-    char Next;          // FALSE or &AdresseMemoire
-    char Previous;      // FALSE or &AdresseMemoire
+    sCard* RightChild;  // FALSE or &AdresseMemoire
+    sCard* LeftChild;   // FALSE or &AdresseMemoire
+    sCard* RightParent; // FALSE or &AdresseMemoire
+    sCard* LeftParent;  // FALSE or &AdresseMemoire
+    sCard* Next;        // FALSE or &AdresseMemoire
+    sCard* Previous;    // FALSE or &AdresseMemoire
 };
-typedef struct Card sCard;
 
 struct TreeCards {
     struct sCard* IdCard;
-    char FirstRightImaginary;   // &AdresseMemoire
-    char FirstLeftImaginary;    // &AdresseMemoire
+    sCard* FirstRightImaginary;   // &AdresseMemoire
+    sCard* FirstLeftImaginary;    // &AdresseMemoire
 };
 typedef struct TreeCards sTreeCards;
 
 struct ListCards {
-    struct sCard* IdCard;
-    char First;         // &AdresseMemoire
-    char Last;          // &AdresseMemoire
+    sCard* First;       // &AdresseMemoire
+    sCard* Last;        // &AdresseMemoire
     int NumberElements; // 1 to (max) 52
 };
 typedef struct ListCards  sListCards;
