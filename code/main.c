dbc206348d5708676542dd8474c7ad7580ff8efa
Pierre Kerzerho <kerzerho.pierre13@gmail.com>
2021-12-14 12:49:40 +0100

    ajout fonction win & modification struct dans .h

--- code/main.c
+++ code/main.c
@@ -1,38 +1,18 @@
-/*
-Romain Gadani
- */
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "struct.h"
+#include "win.h"
 
-struct Card {
-    int Number;         // 1 to 13
-    char* Symbol;       // carreau or pic or trefle or coeur
-    int ReturnedCard;   // 0 or 1
-    int CardSlot;       // 0 or 1 or 2
-    int IdCard;         // 1 to 52
-    char RightChild;    // FALSE or &AdresseMemoire
-    char LeftChild;     // FALSE or &AdresseMemoire
-    char RightParent;   // FALSE or &AdresseMemoire
-    char LeftParent;    // FALSE or &AdresseMemoire
-    char Next;          // FALSE or &AdresseMemoire
-    char Previous;      // FALSE or &AdresseMemoire
-};
-typedef struct Card sCard;
-
-struct TreeCards {
-    struct sCard* IdCard;
-    char FirstRightImaginary;   // &AdresseMemoire
-    char FirstLeftImaginary;    // &AdresseMemoire
-};
-typedef struct TreeCards sTreeCards;
-
-struct ListCards {
-    struct sCard* IdCard;
-    char First;         // &AdresseMemoire
-    char Last;          // &AdresseMemoire
-    int NumberElements; // 1 to (max) 52
-};
-typedef struct ListCards  sListCards;
+int main() {
+    /*
+    //=========win test =========
+    sListCards Reserve;
+    sListCards Fondation;
+    Reserve.NumberElements = 24;
+    Fondation.NumberElements = 25;
+    win(&Reserve, &Fondation);
+    */
+    return 0;
+}
